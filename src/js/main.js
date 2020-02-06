1e541adab90314a9d8b8817e8f5097033b13c816
mazax1ct <vova@giveandget.ru>
2020-02-06 20:25:11 +0300

    half of page

--- src/js/main.js
+++ src/js/main.js
@@ -49,6 +49,19 @@ $(document).ready(function() {
     return false;
   });
 
+  if($('body').width() < 768){
+    if($('.js-list-5').length) {
+      $('.js-list-5').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false,
+        prevArrow: '<button class="slick-arrow slick-prev" aria-label="Назад" type="button"><svg class="slick-arrow__arrow" aria-hidden="true"><use xlink:href="#slider_arrow_left"/></svg></button>',
+        nextArrow: '<button class="slick-arrow slick-next" aria-label="Вперед" type="button"><svg class="slick-arrow__arrow" aria-hidden="true"><use xlink:href="#slider_arrow_right"/></svg></button>',
+        mobileFirst: true
+      });
+    }
+  }
+
   //слайдер игр в блоке выбора конфигурации по fps
   $('.js-fps-slider').slick({
     infinite: false,
