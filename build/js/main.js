530a3c4759395ee21add0f5b914ff73e12c441be
mazax1ct <vova@giveandget.ru>
2020-02-10 14:26:56 +0300

    finish

--- build/js/main.js
+++ build/js/main.js
@@ -51,6 +51,7 @@ $(document).ready(function() {
   });
 
   if($('body').width() < 768) {
+    //слайдер корпусов
     if($('.js-list-1').length) {
       $('.js-list-1').on('init', function(event, slick) {
         var list = $('.js-list-1').find('.slick-dots');
@@ -70,6 +71,27 @@ $(document).ready(function() {
       });
     }
 
+    //слайдер конфигураций
+    if($('.js-list-3').length) {
+      $('.js-list-3').on('init', function(event, slick) {
+        var list = $('.js-list-3').find('.slick-dots');
+        $.each(list['0'].children, function(index, value) {
+          if(index < 9) {
+            list['0'].children[index].children['0'].innerText = '0' + list['0'].children[index].children['0'].innerText;
+          }
+        });
+      });
+
+      $('.js-list-3').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: false,
+        mobileFirst: true
+      });
+    }
+
+    //слайдер дизайна
     if($('.js-list-5').length) {
       $('.js-list-5').slick({
         slidesToShow: 1,
@@ -82,6 +104,7 @@ $(document).ready(function() {
     }
   }
 
+  //слайдер игр
   if($('.js-list-2').length) {
     $('.js-list-2').slick({
       slidesToShow: 1,
